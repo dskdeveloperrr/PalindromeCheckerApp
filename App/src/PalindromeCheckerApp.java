be57ecb43a5f9ef47657c1353c3333a31ba67c56
dskdeveloperrr <sarath.dampuri22@gmailcom>
2026-03-10 09:23:58 +0530

    DSK Added UC10 - Case-Insensitive & Space-Ignored Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -10,31 +10,26 @@ public class PalindromeCheckerApp {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a word: ");
+        System.out.print("Enter a sentence: ");
         String input = sc.nextLine();
 
-        boolean isPalindrome = check(input, 0, input.length() - 1);
+        // Normalize the string
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
         System.out.println("Input: " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
 
         sc.close();
-    }
-
-    // Recursive function to check palindrome
-    private static boolean check(String s, int start, int end) {
-
-        // Base condition
-        if (start >= end) {
-            return true;
-        }
-
-        // If characters do not match
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return check(s, start + 1, end - 1);
     }
 }
